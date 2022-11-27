e51c15335f4178dfa3bb36d6927034a6bc92c1a7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-11-27 13:36:45 +0530

    Update head.js

--- app/head.js
+++ app/head.js
@@ -2,6 +2,8 @@ const Head = () => (
   <>
     <title>Metaversus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Metaversus UI/UX website concept created by Utkarsh Pathrabe using NextJS, TailwindCSS and Framer Motion" />
+    <meta name="keywords" content="Metaversus, Utkarsh Pathrabe, NextJS, TailwindCSS, Framer Motion" />
     <link rel="icon" href="/favicon.ico" />
   </>
 );
